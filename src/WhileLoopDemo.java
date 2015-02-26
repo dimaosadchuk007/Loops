d8e8d1e647f3cbe032ff84ab126978a6ddec5cf6
dimaosadchuk <dimaosadchuk007@gmail.com>
2015-02-26 16:30:07 +0200

    something i trying do with this mess

---
+++ src/WhileLoopDemo.java
@@ -0,0 +1,42 @@
+
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+	
+	String [] friends = new String[20];
+	friends[0]="Masha";
+	friends[1]="Matilda";
+	friends[2]="Rosa";
+	friends[3]=args[0];
+	friends[4]=args[1];
+	friends[18]="Hillary";
+	friends[19]="Natasha";
+	
+	int totalElements = friends.length;
+	int i = 0;
+	
+	  while(i<totalElements){
+		if(friends[i]==null){
+			i++;
+			continue;
+		}
+		if(friends[i]=="Rosa"){
+			//break;
+		}
+		System.out.println("I love "+friends[i]);
+		i++;
+	}
+	for(String girl:friends){
+		if(girl==null){continue;}
+		System.out.println("I love "+girl);
+	}
+	
+	//conditional operator(?)
+	int k = 3;
+	int f = 4;
+	int d = k>f?5:0;
+	System.out.println(d);//0
+	System.out.println("The iteration is over");
+	}
+
+}
